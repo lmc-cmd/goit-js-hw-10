3272a9fb592aefd9552f300c80f9fcc662455522
lmc-cmd <63010584+lmc-cmd@users.noreply.github.com>
2022-07-08 00:06:12 +0300

    +

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import './css/styles.css';
+import countryCard from './templates/coutry_card.hbs';
+import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
+
+// const debounce = require('lodash.debounce');
 
 const DEBOUNCE_DELAY = 300;
+const inputEl = document.querySelector('#search-box');
+
+const onInput = e => {
+  e.priventdefault;
+  console.dir(e.target.value);
+};
+
+inputEl.addEventListener('input', onInput);
+
+// console.log('inputEl :>> ', inputEl);
+
+// debounce(onInput, [(wait = DEBOUNCE_DELAY)], [(options = {})]);
