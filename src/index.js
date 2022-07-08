41231da15076be5a7b74336c2922339858117f6e
lmc-cmd <63010584+lmc-cmd@users.noreply.github.com>
2022-07-08 21:01:27 +0300

    d

--- src/index.js
+++ src/index.js
@@ -2,19 +2,29 @@ import './css/styles.css';
 import countryCard from './templates/coutry_card.hbs';
 import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
-
+import { CountryApi } from './js/fetchCountries';
 // const debounce = require('lodash.debounce');
 
-const DEBOUNCE_DELAY = 300;
+const DEBOUNCE_DELAY = 2000;
 const inputEl = document.querySelector('#search-box');
-
+const countryApi = new CountryApi();
 const onInput = e => {
   e.priventdefault;
-  console.dir(e.target.value);
+  // console.dir(e.target.value);
+  const searchQry = e.target.value;
+  countryApi.searchQry = searchQry;
+  countryApi.fetchCountry().than(data => {
+    console(data);
+  });
 };
+// debounce(onInput, DEBOUNCE_DELAY);
 
-inputEl.addEventListener('input', onInput);
+inputEl.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
 // console.log('inputEl :>> ', inputEl);
 
-// debounce(onInput, [(wait = DEBOUNCE_DELAY)], [(options = {})]);
+// galleryApi.fetchImages().then(data => {
+//     listEl.innerHTML = createGalleryList(data.hits);
+//     moreBtnEl.classList.remove(`is-hidden`);
+//   });
+// };
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,1 +1,52 @@
 // https://restcountries.com/v3.1/name/{name}
+
+import Notiflix from 'notiflix';
+
+export class CountryApi {
+  #BASE_URL = 'https://restcountries.com/v3.1/name/';
+  constructor() {
+    this.searchQry = null;
+  }
+  fetchCountry() {
+    return fetch(`${this.#BASE_URL}${this.searchQry}`)
+      .then(response => {
+        if (!response.ok) {
+          throw `err`;
+        }
+        return response.json();
+      })
+      .catch(err => {
+        Notiflix.Notify.failure(`Err`);
+      });
+  }
+}
+
+// export class GalleryApi {
+//   #BASE_URL = 'https://pixabay.com/api/';
+//   #API_KEY = '13965574-3ae6669f35304ffc6cddc1b72';
+//   constructor() {
+//     this.page = 1;
+//     this.searchQry = null;
+//   }
+//   fetchImages() {
+//     return (
+//       fetch(
+//         `${this.#BASE_URL}?key=${this.#API_KEY}&q=${this.searchQry}&page=${
+//           this.page
+//         }&per_page=12`
+//       )
+//         .then(response => {
+//           if (!response.ok) {
+//             throw 'err';
+//           }
+//           //   console.log(response);
+//           return response.json();
+//         })
+//         //   .then(() => {})
+//         //   .then()
+//         .catch(err => {
+//           console.log(err);
+//         })
+//     );
+//   }
+// }
