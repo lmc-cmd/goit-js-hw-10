a4c927d9d82c84c5d11e5aea8572ec6567f62b19
lmc-cmd <63010584+lmc-cmd@users.noreply.github.com>
2022-07-09 12:31:05 +0300

    a

--- src/index.js
+++ src/index.js
@@ -1,20 +1,32 @@
 import './css/styles.css';
-import createCountryList from './templates/coutry_card.hbs';
-import Notiflix from 'notiflix';
+import createCountryList from './templates/country_list.hbs';
+import createCountryCard from './templates/country_card.hbs';
+import Notiflix, { Notify } from 'notiflix';
 import debounce from 'lodash.debounce';
 import { CountryApi } from './js/fetchCountries';
 // const debounce = require('lodash.debounce');
 
+const listEl = document.querySelector('.country-list');
 const DEBOUNCE_DELAY = 2000;
 const inputEl = document.querySelector('#search-box');
 const countryApi = new CountryApi();
 const onInput = e => {
   e.priventdefault;
+  listEl.innerHTML = null;
   // console.dir(e.target.value);
   const searchQry = e.target.value;
   countryApi.searchQry = searchQry.trim(` `);
-  countryApi.fetchCountry(data => {
-    listEl.innerHTML = createCountryList(data);
+  countryApi.fetchCountry().then(data => {
+    if (data.length > 10) {
+      Notify.info(`Too many matches found. Please enter a more specific name`);
+      return;
+    }
+    if (data.length === 1) {
+      listEl.innerHTML = createCountryCard(data);
+      console.dir();
+    } else {
+      listEl.innerHTML = createCountryList(data);
+    }
   });
 };
 
